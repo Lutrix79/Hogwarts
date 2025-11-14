3049f8b1da3f62507b2638934a6e1633d126837e
Lutrix79 <lutrix79@gmail.com>
2025-11-14 17:27:58 +0300

    Hogwarts DB

---
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,45 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.FacultyRepository;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+public class FacultyService {
+
+    private final FacultyRepository facultyRepository;
+
+    public FacultyService(FacultyRepository facultyRepository) {
+        this.facultyRepository = facultyRepository;
+    }
+
+    public Faculty createFaculty (Faculty faculty) {
+        return facultyRepository.save(faculty);
+    }
+
+    public Faculty getFaculty(long id) {
+        return facultyRepository.findById(id).get();
+    }
+
+    public Faculty editFaculty(Faculty faculty) {
+        return facultyRepository.save(faculty);
+    }
+
+    public void deleteFaculty (long id) {
+        facultyRepository.deleteById(id);
+    }
+
+    public Collection<Faculty> filterFacultyByColor(String color) {
+        return facultyRepository.findAll().stream()
+                .filter(s -> Objects.equals(s.getColor(), color))
+                .collect(Collectors.toCollection(HashSet::new));
+    }
+
+    public Collection<Faculty> allFaculties () {
+        return facultyRepository.findAll();
+    }
+}
---
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -0,0 +1,47 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.StudentRepository;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public Student createStudent (Student student) {
+        return studentRepository.save(student);
+    }
+
+    public Student getStudent(long id) {
+        return studentRepository.findById(id).get();
+    }
+
+    public Student editStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public void deleteStudent (long id) {
+        studentRepository.deleteById(id);
+    }
+
+    public Collection<Student> filterStudentsByAge(int age) {
+        return studentRepository.findAll().stream()
+                .filter(s -> s.getAge() == age)
+                .collect(Collectors.toCollection(HashSet::new));
+    }
+
+    public Collection<Student> allStudents () {
+        return studentRepository.findAll();
+    }
+}
