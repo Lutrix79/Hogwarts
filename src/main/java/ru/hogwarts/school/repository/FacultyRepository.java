4d3df4b3d93fe53bec51e7a5524540fd65e694c9
Lutrix79 <lutrix79@gmail.com>
2025-11-17 17:32:39 +0300

    SQL Requests

--- src/main/java/ru/hogwarts/school/repository/FacultyRepository.java
+++ src/main/java/ru/hogwarts/school/repository/FacultyRepository.java
@@ -2,7 +2,14 @@ package ru.hogwarts.school.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
+
+import java.util.Collection;
 
 public interface FacultyRepository extends JpaRepository<Faculty, Long> {
+
+    Faculty findFacultyByNameOrColorIgnoreCase(String name, String color);
+
+    Collection<Student> getStudentsByFacultyId(Long facultyId);
 
 }
--- src/main/java/ru/hogwarts/school/repository/StudentRepository.java
+++ src/main/java/ru/hogwarts/school/repository/StudentRepository.java
@@ -1,8 +1,15 @@
 package ru.hogwarts.school.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
 
+import java.util.Collection;
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    Collection<Student> findByAgeBetween(Long minAge, Long maxAge);
+
+    Faculty getFacultyByStudentId (Long studentId);
 
 }
